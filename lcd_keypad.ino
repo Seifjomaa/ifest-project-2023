defbb267fb22f756f72e1bba239673e8a38a2b79
Seifjomaa <124698967+Seifjomaa@users.noreply.github.com>
2023-02-07 15:18:58 +0000

    Add files via upload

---
+++ lcd_keypad.ino
@@ -0,0 +1,56 @@
+#include<LiquidCrystal_I2C_Hangul.h>
+#include<Wire.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+LiquidCrystal_I2C_Hangul lcd(0x27,16,2); //LCD 클래스 초기화
+#include <Keypad.h>
+
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {5,6,7,8}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {2,3,4}; //connect to the column pinouts of the keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+void setup(){
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  delay(4);       // Optional delay. Some board do need more time after init to be ready, see Readme
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  lcd.print(F("card UID"));
+
+}
+  
+void loop(){
+  char key = keypad.getKey();
+ if (key){
+  lcd.setCursor(0,1);
+   lcd.print(key);}
+      // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  // Dump debug info about the card; PICC_HaltA() is automatically called
+  mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+  
+}
